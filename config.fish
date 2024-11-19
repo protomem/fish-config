cfb8408a108c19b37537a4165d4f48c94c9831fa
protomem <protomem0709@gmail.com>
2024-11-19 14:53:37 +0300

    git init

---
+++ config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+
+set -Ux EDITOR hx
+
+set -U fish_user_paths $fish_user_paths /usr/local/bin $HOME/.local/bin /usr/local/go/bin $HOME/go/bin $HOME/.zig $HOME/.cache/rebar3/bin
+
+alias vim nvim
+alias g git
+alias k kubectl
+
+source ~/.asdf/asdf.fish
