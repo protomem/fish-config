2c6d406c611b081319c8f2cb549ebdd8bbe0725a
protomem <protomem0709@gmail.com>
2024-12-04 13:00:26 +0300

    update config

---
+++ completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/protomem/.asdf/completions/asdf.fish
--- config.fish
+++ config.fish
@@ -2,11 +2,10 @@ set fish_greeting
 
 set -Ux EDITOR hx
 
-set -U fish_user_paths $fish_user_paths /usr/local/bin $HOME/.local/bin /usr/local/go/bin $HOME/go/bin $HOME/.zig $HOME/.cache/rebar3/bin
+set -U fish_user_paths $fish_user_paths /usr/local/bin $HOME/.local/bin /usr/local/go/bin $HOME/go/bin $HOME/.cache/rebar3/bin
 
 alias vim nvim
 alias g git
 alias k kubectl
 
 source ~/.asdf/asdf.fish
-oh-my-posh init fish --config /home/protomem/.cache/oh-my-posh/themes/onehalf.minimal.omp.json | source
